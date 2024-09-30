e63f184bb8a22bf0b3158d7b7d4408827ce18b2a
LJ Solana <111569336+LJ-Solana@users.noreply.github.com>
2024-09-30 08:17:26 +0400

    blockMeta

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const ClientWalletProvider = dynamic(
 )
 
 export const metadata: Metadata = {
-  title: 'Byte News | On-Chain Verified News',
+  title: 'Byte News | Blockchain Verified News',
   description: 'Stay updated with global, economically verified truthful news.',
 }
 
